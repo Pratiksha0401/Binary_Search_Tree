2adff1cb9d283df0de2fa4946ffdc662230f0d49
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-05-10 08:24:07 +0530

    Added: code to create binary tree as shown in figure

--- src/binarySearchTree/myBinarySearchTree.java
+++ src/binarySearchTree/myBinarySearchTree.java
@@ -7,9 +7,26 @@ public class myBinarySearchTree {
 		tree.add(56);
 		tree.add(30);
 		tree.add(70);
+		tree.add(56);
+		tree.add(30);
+		tree.add(22);
+		tree.add(11);
+		tree.add(3);
+		tree.add(16);
+		tree.add(40);
+		tree.add(70);
+		tree.add(60);
+		tree.add(65);
+		tree.add(63);
+		tree.add(67);
+		tree.add(95);
 		
 		 tree.inorder();
-}
+		 
+		 System.out.println("\nThe size of Binary tree is : "+ tree.size());
+		 
+		
+	}
 }
 
 class BinarySearchTree {
@@ -33,6 +50,21 @@ class BinarySearchTree {
 	         root = addRecurrsivly(root, key);
 	 }
 	 
+	public  Node addRecurrsivly(Node root, int key) {
+			   
+		if (root == null) {
+		        
+			root = new Node(key);
+		    return root;
+		}
+		if (key < root.key)
+		    root.left = addRecurrsivly(root.left, key);
+		else if (key > root.key)
+		    root.right =addRecurrsivly(root.right, key);
+		
+		return root;
+		 }	 
+	 
 	 public  void inorder()  {
 		 
          inorderAt(root);
@@ -47,17 +79,16 @@ class BinarySearchTree {
 	        }
 	 }
 
-	public  Node addRecurrsivly(Node root, int key) {
-	   
-		 if (root == null) {
-	        
-	            root = new Node(key);
-	            return root;
-	     }
-		 if (key < root.key)
-	            root.left = addRecurrsivly(root.left, key);
-	     else if (key > root.key)
-	            root.right =addRecurrsivly(root.right, key);
-		 return root;
+	
+	public int size() {
+		 return sizeOfNode(root);
 	 }
+	 
+	 public int sizeOfNode(Node root) {
+		 if(root == null)
+			 return 0;
+		 else
+			 return(sizeOfNode(root.left )+ 1 + sizeOfNode(root.right));
+	 }
+	 
 }
