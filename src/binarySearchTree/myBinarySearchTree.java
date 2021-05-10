21b77755622922cf70b64c9e50a03cf1e20d7ac0
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-05-10 08:27:24 +0530

    Added: code to search 63 in binary tree

--- src/binarySearchTree/myBinarySearchTree.java
+++ src/binarySearchTree/myBinarySearchTree.java
@@ -25,7 +25,7 @@ public class myBinarySearchTree {
 		 
 		 System.out.println("\nThe size of Binary tree is : "+ tree.size());
 		 
-		
+		 tree.search(63);
 	}
 }
 
@@ -89,6 +89,35 @@ class BinarySearchTree {
 			 return 0;
 		 else
 			 return(sizeOfNode(root.left )+ 1 + sizeOfNode(root.right));
+	 }
+	 
+	 static boolean checkIfExist( Node node, int key)
+	    {
+	        if (node == null)
+	            return false;
+	     
+	        if (node.key == key)
+	            return true;
+	     
+	        // then recur on left subtree
+	        boolean res1 = checkIfExist(node.left, key);
+	       
+	        // node found, no need to look further
+	        if(res1) return true;
+	        
+	        // node is not found in left, so recur on right subtree
+	        boolean res2 = checkIfExist(node.right, key);
+	     
+	        return res2;
+	    }
+	 
+	 public void search(int key) {
+	     
+	     if (checkIfExist(root, key))
+	         System.out.println( key +" exists");
+	     else
+	         System.out.println(key + " does not exits");
+		 
 	 }
 	 
 }
